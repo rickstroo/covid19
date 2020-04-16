77b2fe00a10fb675bf15f4b55839cab24d4637b1
Rick Stroobosscher <Rick@ricks-mbp.lan>
2020-04-16 14:53:05 -0400

    A little more clean up.

--- dcmdmp.py
+++ dcmdmp.py
@@ -1,12 +1,23 @@
 import argparse
-import pydicom
 
 parser = argparse.ArgumentParser(description='Extracts measurements from DICOM SR as XML and outputs as JSON.')
 parser.add_argument("-f", "--file", required=True, type=str, help="The name of the file to parse")
 args = parser.parse_args()
 
 file = args.file
+
+import pydicom
+
 dataset = pydicom.dcmread('/Users/Rick/data/dicom/GENECG.dcm')
 print(dataset)
 acn = dataset[0x0008,0x0050]
 print(acn)
+
+from xml.etree.ElementTree import fromstring as fs
+import xmljson
+from xmljson import badgerfish as bf
+import json
+
+xml = '<employees><person><name value="Alice"/></person><person><name value="Bob"/></person></employees>'
+print(xml)
+print(json.dumps(bf.data(fs(xml))))
