f134fcc3696045e73d42569cdc64ee958306e0d5
Rick Stroobosscher <Rick@ricks-mbp.lan>
2020-04-16 14:02:05 -0400

    Cleaning up.

--- parse.py
+++ parse.py
@@ -1,12 +1,15 @@
 import argparse
-import pydicom
 
 parser = argparse.ArgumentParser(description='Extracts measurements from DICOM SR as XML and outputs as JSON.')
 parser.add_argument("-f", "--file", required=True, type=str, help="The name of the file to parse")
 args = parser.parse_args()
 
 file = args.file
-dataset = pydicom.dcmread('/Users/Rick/data/dicom/GENECG.dcm')
+
+dataset = pydicom.dcmread(file)
 print(dataset)
 acn = dataset[0x0008,0x0050]
 print(acn)
+
+str = '<employees><person><name value="Alice"/></person><person><name value="Bob"/></person></employees>'
+print(str)
