aeed9f5ecc77c4412c940c8e69b5baa98dcbc062
Rick Stroobosscher <Rick@ricks-mbp.lan>
2020-04-16 14:02:54 -0400

    Cleaning up.

--- xml2json.py
+++ xml2json.py
@@ -1,3 +1,8 @@
+import xmljson
+from xml.etree.ElementTree import fromstring
+from json import dumps
+from xmljson import badgerfish
+
 import argparse
 
 parser = argparse.ArgumentParser(description='Extracts measurements from DICOM SR as XML and outputs as JSON.')
@@ -6,10 +11,10 @@ args = parser.parse_args()
 
 file = args.file
 
-dataset = pydicom.dcmread(file)
-print(dataset)
-acn = dataset[0x0008,0x0050]
-print(acn)
+print(file)
 
 str = '<employees><person><name value="Alice"/></person><person><name value="Bob"/></person></employees>'
 print(str)
+xml = badgerfish.data(fromstring(str))
+json = dumps(xml)
+print(json)
