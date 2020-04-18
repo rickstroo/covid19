bd8a1bc23ce9ac9b224a84a6d9c1b343872eb9f2
Rick Stroobosscher <Rick@ricks-mbp.lan>
2020-04-18 14:32:43 -0400

    Adding support for FHIR resources.

--- dcmdmp.py
+++ dcmdmp.py
@@ -6,6 +6,8 @@ args = parser.parse_args()
 
 file = args.file
 
+# import dicom library
+# requires "pip3.8 install pydicom"
 import pydicom
 
 dataset = pydicom.dcmread('/Users/Rick/data/dicom/GENECG.dcm')
@@ -18,6 +20,25 @@ import xmljson
 from xmljson import badgerfish as bf
 import json
 
-xml = '<employees><person><name value="Alice"/></person><person><name value="Bob"/></person></employees>'
+xml = '<organization><id>GRH</id><name>Grand River Hospital</name><address><country>Canada</country></address></organization>'
 print(xml)
-print(json.dumps(bf.data(fs(xml))))
+obj = fs(xml)
+print(obj)
+print(json.dumps(bf.data(obj)))
+
+# import fhir resources
+# requires "pip3.8 install fhir.resources"
+from fhir.resources.organization import Organization
+from fhir.resources.address import Address
+
+# create a fhir resource the "python" way
+org = Organization()
+org.id = "GRH"
+org.name = "Grand River Hospital"
+org.address = list()
+addr = Address()
+addr.country = "Canada"
+org.address.append(addr)
+
+# print the fhir resource as a JSON object
+print(org.as_json())
