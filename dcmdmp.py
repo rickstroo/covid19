6d783aa23872fd6f0f199617ab98455ba1cfe65a
Rick Stroobosscher <Rick@ricks-mbp.lan>
2020-04-19 13:45:08 -0400

    Created a sample blood pressure measurement using JSON.

--- dcmdmp.py
+++ dcmdmp.py
@@ -61,31 +61,111 @@ print(org.as_json())
 from fhir.resources.observation import Observation
 from fhir.resources.codeableconcept import CodeableConcept
 from fhir.resources.coding import Coding
+from fhir.resources.fhirreference import FHIRReference
+from fhir.resources.identifier import Identifier
 
-# create an observation
-obs = Observation()
-obs.id = '1' # should be generated
-obs.status = 'final'
-obs.category = list()
-category = CodeableConcept()
-category.coding = list()
-code = Coding()
-code.system = 'http://hl7.org/fhir/ValueSet/observation-category'
-code.version = '4.0.1'
-code.code = 'imaging'
-code.display = 'imaging'
-category.coding.append(code)
-obs.category.append(category)
-obs.code = CodeableConcept()
-obs.code.coding = list()
-code = Coding()
-code.system = 'unknown'
-code.version = 'unknown'
-code.code = 'unknown'
-code.display = 'pleural thickening'
-obs.code.coding.append(code)
+# create an observation using json
 
-# more to do...
+json = {
+  "resourceType": "Observation",
+  "id": "blood-pressure",
+  "meta": {
+    "lastUpdated": "2014-01-30T22:35:23+11:00"
+  },
+  "text": {
+    "status": "generated",
+    "div": "<div><p><b>Generated Narrative with Details</b></p><p><b>id</b>: blood-pressure</p><p><b>meta</b>: </p><p><b>identifier</b>: urn:uuid:187e0c12-8dd2-67e2-99b2-bf273c878281</p><p><b>status</b>: final</p><p><b>code</b>: Blood pressure systolic &amp; diastolic <span>(Details : {LOINC code '55284-4' = 'Blood pressure systolic and diastolic', given as 'Blood pressure systolic &amp; diastolic'})</span></p><p><b>subject</b>: <a>Patient/example</a></p><p><b>effective</b>: 17/09/2012</p><p><b>performer</b>: <a>Practitioner/example</a></p><p><b>interpretation</b>: low <span>(Details : {http://hl7.org/fhir/v2/0078 code 'L' = 'Low', given as 'Below low normal'})</span></p><p><b>bodySite</b>: Right arm <span>(Details : {SNOMED CT code '368209003' = '368209003', given as 'Right arm'})</span></p><blockquote><p><b>component</b></p><p><b>code</b>: Systolic blood pressure <span>(Details : {LOINC code '8480-6' = 'Systolic blood pressure', given as 'Systolic blood pressure'}; {SNOMED CT code '271649006' = '271649006', given as 'Systolic blood pressure'}; {http://acme.org/devices/clinical-codes code 'bp-s' = '??', given as 'Systolic Blood pressure'})</span></p><p><b>value</b>: 107 mm[Hg]</p></blockquote><blockquote><p><b>component</b></p><p><b>code</b>: Diastolic blood pressure <span>(Details : {LOINC code '8462-4' = 'Diastolic blood pressure', given as 'Diastolic blood pressure'})</span></p><p><b>value</b>: 60 mm[Hg]</p></blockquote></div>"
+  },
+  "identifier": [
+    {
+      "system": "urn:ietf:rfc:3986",
+      "value": "urn:uuid:187e0c12-8dd2-67e2-99b2-bf273c878281"
+    }
+  ],
+  "status": "final",
+  "code": {
+    "coding": [
+      {
+        "system": "http://loinc.org",
+        "code": "55284-4",
+        "display": "Blood pressure systolic & diastolic"
+      }
+    ]
+  },
+  "subject": {
+    "reference": "Patient/example"
+  },
+  "effectiveDateTime": "2012-09-17",
+  "performer": [
+    {
+      "reference": "Practitioner/example"
+    }
+  ],
+  "interpretation": [
+    {
+        "coding": [
+        {
+            "system": "http://hl7.org/fhir/v2/0078",
+            "code": "L",
+            "display": "Below low normal"
+            }
+            ],
+            "text": "low"
+            }
+        ],
+  "bodySite": {
+    "coding": [
+      {
+        "system": "http://snomed.info/sct",
+        "code": "368209003",
+        "display": "Right arm"
+      }
+    ]
+  },
+  "component": [
+    {
+      "code": {
+        "coding": [
+          {
+            "system": "http://loinc.org",
+            "code": "8480-6",
+            "display": "Systolic blood pressure"
+          },
+          {
+            "system": "http://snomed.info/sct",
+            "code": "271649006",
+            "display": "Systolic blood pressure"
+          },
+          {
+            "system": "http://acme.org/devices/clinical-codes",
+            "code": "bp-s",
+            "display": "Systolic Blood pressure"
+          }
+        ]
+      },
+      "valueQuantity": {
+        "value": 107,
+        "unit": "mm[Hg]"
+      }
+    },
+    {
+      "code": {
+        "coding": [
+          {
+            "system": "http://loinc.org",
+            "code": "8462-4",
+            "display": "Diastolic blood pressure"
+          }
+        ]
+      },
+      "valueQuantity": {
+        "value": 60,
+        "unit": "mm[Hg]"
+      }
+    }
+  ]
+}
+obs = Observation(json)
 
 # print the observation as a JSON object
 print(obs.as_json())
